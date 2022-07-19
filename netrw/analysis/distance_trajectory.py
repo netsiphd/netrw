b6cd41c69bec7e69e7a1caf99f552c212a0b3b62
Alice Schwarze <aliceschwarze@gmail.com>
2022-07-19 11:14:24 -0400

    added networkx edge swap method

--- netrw/analysis/distance_trajectory.py
+++ netrw/analysis/distance_trajectory.py
@@ -2,29 +2,72 @@ from matplotlib import pyplot
 import networkx
 import netrd
 
-
-
-def distanceTrajectory(G, distance=..., rewire=..., null_model=None, 
-    num_rewire=100, num_networks=100, distance_kwargs={}, rewire_kwargs={}, 
+def distanceTrajectory(G, distance=netrd.distance.Hamming, rewire=netrw.rewire.KarrerRewirer, null_model=None, 
+    num_rewire=100, num_runs=100, distance_kwargs={}, rewire_kwargs={}, 
     null_model_kwargs={}, **kwargs):
+    '''
+    Get some data on graph distances as a function of number of rewiring steps.
+    
+    Parameters
+    ----------
+    G : networkx Graph or DiGraph
+    
+    distance : netrd graph distance class
+    
+    rewire : netrw rewire class
+    
+    null_model : ???
+    
+    num_rewire : integer or list
+       number of rewiring steps to be tracked or ordered list of rewiring
+       steps to be tracked
+       
+    num_runs : integer
+       number of trajectories to be generated for evaluating the standard 
+       deviation for a set of rewiring trajectories
+       
+    distance_kwargs : dictionary
+       a dictionary of keyword arguments for an instantiation of the netrd
+       distance class
+       
+    rewire_kwargs : dictionary
+       a dictionary of keyword arguments for an instantiation of
+       the netrw rewire class
+       
+    null_model_kwargs : dictionary
+       a dictionary of keyword arguments for the null model (?)
+    
+    '''
 
     G0 = copy.deepcopy(G)
     
+    # check whether input for num rewire in a number of rewiring steps (int)
+    # or a list of steps
     if hasattr(num_rewire, "__iter__"):
         rewire_steps = num_rewire
     else:
         rewire_steps = range(num_rewire)
         
-    data = np.zeros(len(rewire_steps))
+    # initialize data array
+    data = np.zeros(len(rewire_steps))    
     
-    for i, r in enumerate(rewire_steps):
-       rewire(G0, **rewire_kwargs)
-       data[i] = distance(G0, G, **distance_kwargs)
+    # define a rewire function
+    step_rewire = rewire().step
+    rewire_function = lambda g : step_rewire(g, copy_graph=False, **rewire_kwargs)
+    
+    # define a distance function
+    distfun = distance() # get a class instantiation
+    distance_function = lambda g1, g2: distfun(g1, g2, **distance_kwargs)
+    
+    for i in range(max(rewire_steps)):
+       rewire_function(G0)
+       data[i+1] = distance(G0, G)
 
     return data
 
+
 def plotDistanceTrajectory(G, **kwargs):
     
-    data = distanceTrajectory(G, kwargs**)
+    data = distanceTrajectory(G, **kwargs)
     
     
---
+++ netrw/rewire/networkXEdgeSwap.py
@@ -0,0 +1,28 @@
+from . import BaseRewirer
+import copy
+import itertools as it
+import random
+import networkx as nx
+
+
+class NetworkXEdgeSwap(BaseRewirer):
+    """Perturb one edge of node `i` in the way described by Karrer et al. (2008).
+    Choose an edge incident on `i` at random; delete it and replace it with
+    a new edge that is not already present in the network (and is not
+    necessarily incident on `i`).
+
+    Karrer, Brian, Elizaveta Levina, and
+    M. E. J. Newman. 2008. “Robustness of Community Structure in
+    Networks.” Physical Review E 77
+    (4). https://doi.org/10.1103/PhysRevE.77.046119.
+
+    """
+
+    def rewire(self, G, copy_graph=True):
+        
+        if copy_graph:
+            G = copy.deepcopy(G)
+            
+        nx.double_edge_swap(G, nswap=1)
+
+        return G
