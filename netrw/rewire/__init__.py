44a364fada08e1ce880546f72a0b2a3a1bd42589
Brennan Klein <brennanjamesklein@gmail.com>
2022-07-19 16:08:40 -0400

    add local edge rewiring

--- netrw/rewire/__init__.py
+++ netrw/rewire/__init__.py
@@ -1,4 +1,6 @@
 from .base import BaseRewirer
 from .algebraic_connectivity import AlgebraicConnectivity
+from .global_rewiring import GlobalRewiring
+from .local_edge_rewire import LocalEdgeRewirer
 
 __all__ = []
