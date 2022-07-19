9f956936bd129e152e700c1388cd47c1e8ad2b58
Cory Glover <coryglover@Summers-MacBook-Air.local>
2022-07-19 11:01:46 -0400

    added connected watts strogatz method;

--- netrw/rewire/__init__.py
+++ netrw/rewire/__init__.py
@@ -1,5 +1,6 @@
 from .base import BaseRewirer
 from .algebraic_connectivity import AlgebraicConnectivity
 from .watts_strogatz import WattsStrogatz
+from .connected_watts_strogatz import ConnectedWattsStrogatz
 
 __all__ = []
