6d36075c592625abf0de3eda4e6581bf5831c1d3
nwlandry <nicholas.landry.91@gmail.com>
2022-07-20 15:17:32 -0400

    update __init__

--- netrw/rewire/__init__.py
+++ netrw/rewire/__init__.py
@@ -4,5 +4,6 @@ from .algebraic_connectivity import AlgebraicConnectivity
 from .networkXEdgeSwap import NetworkXEdgeSwap
 from .global_rewiring import GlobalRewiring
 from .local_edge_rewire import LocalEdgeRewiring
+from .robust_rewiring import RobustRewirer
 
 __all__ = []
