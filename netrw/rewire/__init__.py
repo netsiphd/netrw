8f745ebc15ffc8bf89a92eed19f3f01b0d0d51f6
Brennan Klein <brennanjamesklein@gmail.com>
2022-07-20 10:08:50 -0400

    fixed import error for local_edge_rewire in __init__

--- netrw/rewire/__init__.py
+++ netrw/rewire/__init__.py
@@ -1,6 +1,6 @@
 from .base import BaseRewirer
 from .algebraic_connectivity import AlgebraicConnectivity
 from .global_rewiring import GlobalRewiring
-from .local_edge_rewire import LocalEdgeRewirer
+from .local_edge_rewire import LocalEdgeRewiring
 
 __all__ = []
