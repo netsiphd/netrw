24c97389f5328aa4ad56934a20aab4707d620037
nwlandry <nicholas.landry.91@gmail.com>
2022-07-20 15:18:27 -0400

    added imports

--- netrw/rewire/__init__.py
+++ netrw/rewire/__init__.py
@@ -1,9 +1,9 @@
-from .base import BaseRewirer
 from .karrer import KarrerRewirer
-from .algebraic_connectivity import AlgebraicConnectivity
-from .networkXEdgeSwap import NetworkXEdgeSwap
 from .global_rewiring import GlobalRewiring
 from .local_edge_rewire import LocalEdgeRewiring
+from .algebraic_connectivity import AlgebraicConnectivity
+from .randomized_weights import RandomizedWeightCM_swap
+from .randomized_weights import RandomizedWeightCM_redistribution
 from .robust_rewiring import RobustRewirer
 
 __all__ = []
