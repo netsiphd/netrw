49a7ab8b7d80d6e1995d84031718a2e67847155b
Cory Glover <coryglover@Summers-MacBook-Air.local>
2022-07-20 11:48:37 -0400

    robust branch

--- netrw/rewire/__init__.py
+++ netrw/rewire/__init__.py
@@ -1,6 +1,7 @@
 from .base import BaseRewirer
 from .global_rewiring import GlobalRewiring
 from .local_edge_rewire import LocalEdgeRewiring
+from .robust_rewiring import RobustRewirer
 
 # from .algebraic_connectivity import AlgebraicConnectivity
 
