77805724c913182c1ca9366c13ee29c50c964307
Harrison Hartle <hh@Harrisons-MacBook-Pro.local>
2022-07-20 13:16:09 -0600

    added to __init__

--- netrw/rewire/__init__.py
+++ netrw/rewire/__init__.py
@@ -1,3 +1,10 @@
 from .base import BaseRewirer
+from .karrer import KarrerRewirer
+from .global_rewiring import GlobalRewiring
+from .local_edge_rewire import LocalEdgeRewiring
+from .assortative import DegreeAssortativeRewirer
+from .algebraic_connectivity import AlgebraicConnectivity
+from .randomized_weights import RandomizedWeightCM_swap
+from .randomized_weights import RandomizedWeightCM_redistribution
 
 __all__ = []
