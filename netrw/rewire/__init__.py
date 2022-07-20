ffbadafeb84cf855cbfbcd2aed4b17b85dded00e
Cory Glover <coryglover@Summers-MacBook-Air.local>
2022-07-20 11:45:06 -0400

    fix init

--- netrw/rewire/__init__.py
+++ netrw/rewire/__init__.py
@@ -1,5 +1,4 @@
 from .base import BaseRewirer
-rom .karrer import KarrerRewirer
 from .global_rewiring import GlobalRewiring
 from .local_edge_rewire import LocalEdgeRewiring
 from .spatial_small_worlds import SpatialSmallWorld
