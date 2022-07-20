0c9a44f7a01e9c9f269ec83dd1dffd0759dd4cdd
hartle <32047935+hartle@users.noreply.github.com>
2022-07-20 13:22:30 -0600

    Adding assortativity-based rewiring method (#20)
    * added assortativity-enhancing/reducing rewiring algorithm
    
    * reformatting
    
    * updated according to pull request comments
    
    * bug fix
    
    * added verbose to full rewire
    
    * added to __init__
    
    Co-authored-by: Harrison Hartle <hh@harrisons-mbp.lan>
    Co-authored-by: Brennan Klein <brennanjamesklein@gmail.com>
    Co-authored-by: Harrison Hartle <hh@Harrisons-MacBook-Pro.local>

---
+++ netrw/rewire/assortative.py
@@ -0,0 +1,88 @@
+from . import BaseRewirer
+import copy
+import random
+import networkx as nx
+import numpy as np
+
+
+class DegreeAssortativeRewirer(BaseRewirer):
+
+    """
+        Do degree-preserving rewiring that increases/decreases assortativity
+        as described in CHANGING CORRELATIONS IN NETWORKS: ASSORTATIVITY AND DISSORTATIVITY,
+        R. Xulvi-Brunet and I.M. Sokolov
+
+    """
+
+    def step_rewire(self, G, p=0.5, assortative=True, copy_graph=True, verbose=False):
+
+        """
+        Inputs:
+            p (float) -- the probability of making the swap be in favor of
+                            increasing/decreasing assortativity. Otherwise,
+                            the swap is of the form (i,j),(k,l)-->(i,l),(j,k).
+
+            assortative (bool) -- if assortative==True, the non-random swaps
+                                    favor increasing assortativity. Otherwise,
+                                    they favor increasing disassortativity.
+        """
+
+        if copy_graph:
+            G = copy.deepcopy(G)
+
+        (i, j), (k, l) = random.sample(list(G.edges), 2)
+
+        # repeat until a valid rewiring is found
+        valid = False
+        while not valid:
+
+            if np.random.rand() <= p:
+
+                # degree-sorting for edge-swap with probability p
+                sor = sorted([i, j, k, l], key=lambda y: G.degree[y])
+
+                if assortative:
+                    I, J, K, L = sor
+                else:
+                    I, J, K, L = sor[0], sor[3], sor[1], sor[2]
+
+            else:
+
+                # standard edge-swap with probability 1-p
+                I, J, K, L = i, l, j, k
+
+            # make sure new edges aren't already there
+            if (not G.has_edge(I, J)) and (not G.has_edge(K, L)):
+                valid = True
+
+                # remove previous edge
+                G.remove_edge(i, j)
+                G.remove_edge(k, l)
+                G.add_edge(I, J)
+                G.add_edge(K, L)
+
+            if verbose:
+                removed_edges = [(i, j), (k, l)]
+                added_edges = [(I, J), (K, L)]
+
+        if verbose:
+            return G, removed_edges, added_edges
+        else:
+            return G
+
+    def full_rewire(self, G, timesteps=1000, p=0.5, assortative=True, copy_graph=True, verbose=False):
+        """
+        Runs step_rewire for a number of steps (default 1000 for no reason)
+        """
+        if verbose:
+            removed_edges = {}
+            added_edges = {}
+            for t in range(timesteps):
+                G,removed,added = self.step_rewire(G, p=p, assortative=assortative, copy_graph=copy_graph,verbose=True)
+                removed_edges[t] = removed
+                added_edges[t] = added
+        else:
+            for t in range(timesteps):
+                G = self.step_rewire(G, p=p, assortative=assortative, copy_graph=copy_graph)
+
+        return G
