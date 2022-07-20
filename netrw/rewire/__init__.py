bfa90fa16b3b0647c56db3ca0e5799bab463fead
Cory Glover <coryglover@Summers-MacBook-Air.local>
2022-07-20 11:23:58 -0400

    updates

--- netrw/rewire/__init__.py
+++ netrw/rewire/__init__.py
@@ -1,6 +1,7 @@
 from .base import BaseRewirer
-from .algebraic_connectivity import AlgebraicConnectivity
 from .global_rewiring import GlobalRewiring
 from .local_edge_rewire import LocalEdgeRewiring
+
+# from .algebraic_connectivity import AlgebraicConnectivity
 
 __all__ = []
